77ef4a83994e02dc2eff96e5510cddaf8f6b310f
Lawrence McDaniel <lpm0073@gmail.com>
2017-04-03 06:25:37 -0500

    added popups and popovers to google chart page

--- src/public/charts/charts.controller.js
+++ src/public/charts/charts.controller.js
@@ -25,14 +25,14 @@
     .directive('gcPiechart', gcPiechart)
     .directive('gcAreachart', gcAreachart)
 
-    ChartsController.$inject = ['$scope', '$log'];
-    function ChartsController($scope, $log) {
+    ChartsController.$inject = ['$scope', '$log', '$sce'];
+    function ChartsController($scope, $log, $sce) {
         console.log('ChartsController instantiated');
         var ctrl = this;
 
         /* ---------- initializatoin of closeable alert ----------*/
         $scope.alerts = [
-          { type: 'danger', msg: 'Did you know? All of these charts are made with HTML5, and, they are highly interactive. Hover your cursor over the graph to see popout annotations and graph animations.' },
+          { type: 'danger', msg: 'Did you know? All of these charts are made with HTML5, and, they are highly interactive. Hover your cursor over the graph to see popout annotations and graph animations.' }
         ];
 
         $scope.addAlert = function() {
@@ -44,7 +44,44 @@
         };
         /* ---------- end initializatoin of closeable alert ----------*/
 
-    }
+        /* ---------- tooltip initializations ----------*/
+
+        $scope.dynamicTooltip = 'Hello, World!';
+        $scope.dynamicTooltipText = 'dynamic';
+        $scope.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
+        $scope.placement = {
+          options: [
+            'top',
+            'top-left',
+            'top-right',
+            'bottom',
+            'bottom-left',
+            'bottom-right',
+            'left',
+            'left-top',
+            'left-bottom',
+            'right',
+            'right-top',
+            'right-bottom'
+          ],
+          selected: 'top'
+        };
+        /* ---------- tooltip initializations ----------*/
+
+        /* ---------- popover initializations ----------*/
+/*        $scope.htmlPopover = $sce.trustAsHtml('<b style="color: red">I can</b> have <div class="label label-success">HTML</div> content');  */
+
+/* <img href="/images/favicon.jpg"> */
+        var htmlPrefix = '<div class="chart-html-popover"><img src="http://cdn.lawrencemcdaniel.com/angry-nun.jpg" alt="Angry Nun" width="75">';
+        var htmlSuffix = '</div>';
+        $scope.htmlPopoverGeo = $sce.trustAsHtml(htmlPrefix + 'There are near infinite ways to colorize and illuminate your geographical data. This example uses scaled shading.' + htmlSuffix);
+        $scope.htmlPopoverArea = $sce.trustAsHtml(htmlPrefix + 'Line charts are a powerful information tool when used effectively. Anything that you can do with Excel or Powerpoint can be done even better when leveraging the power of the Internet and HTML5.' + htmlSuffix);
+        $scope.htmlPopoverCandlestick = $sce.trustAsHtml(htmlPrefix + "This is a popular style for presenting stock high-low-close data. Did you know that most markets as well as providers like Yahoo provide free API's to securities market data?" + htmlSuffix);
+
+      /* ---------- popover initializations ----------*/
+
+
+  }  /* ChartsController() */
 
     gcAreachart.$inject = [];
     function gcAreachart() {
