9223e89507dc61e44b905ecb7a3231a9f5c1a532
Lawrence McDaniel <lpm0073@gmail.com>
2017-04-04 14:00:46 -0500

    replaced site wait spinner svg

--- src/common/loading/loading.component.js
+++ src/common/loading/loading.component.js
@@ -1,21 +1,9 @@
-/*
-Spinner documentation : https://github.com/SamHerbert/SVG-Loaders
-
-<img src="svg-loaders/puff.svg" />
-An icon's color can be manipulated by changing the fill attribute in the SVG file.
-
-<svg fill="#fff" width="140" height="64" viewBox="0 0 140 64" xmlns="http://www.w3.org/2000/svg">
-
-Note: Not all browsers support SVG, or more specifically, animated SVGs. Check (http://caniuse.com/#feat=svg-smil) to make sure you're good to go. If not, you may want to implement a fallback.
-
-*/
-
 (function() {
 "use strict";
 
 angular.module('common')
 .component('loading', {
-  template: '<img src="images/rings.svg" ng-if="$ctrl.show">',
+  template: '<img src="images/gears.svg" ng-if="$ctrl.show">',
   controller: LoadingController
 });
 
