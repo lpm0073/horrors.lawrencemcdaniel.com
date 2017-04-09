4e25d92a38a067de794118cfe8e806ebc6c4136b
Lawrence McDaniel <lpm0073@gmail.com>
2017-04-09 07:52:22 -0500

    setup first pop over on nemo form

--- src/public/text/nemoform/txt.nemoform.controller.js
+++ src/public/text/nemoform/txt.nemoform.controller.js
@@ -4,8 +4,8 @@
     angular.module('public')
     .controller('NemoformController', NemoformController);
 
-    NemoformController.$inject = ['NemoService', '$scope', '$http', 'MSCognitiveService', 'GoogleApiService'];
-    function NemoformController(NemoService, $scope, $http, MSCognitiveService, GoogleApiService) {
+    NemoformController.$inject = ['NemoService', '$scope', '$http', '$sce', 'MSCognitiveService', 'GoogleApiService'];
+    function NemoformController(NemoService, $scope, $http, $sce, MSCognitiveService, GoogleApiService) {
       console.log('NemoformController instantiated');
         var ctrl = this;
         ctrl.userInfo = {};
@@ -14,6 +14,11 @@
         ctrl.message = '';
         ctrl.response = '';
         ctrl.userInfo.incidentReport = 'Ha vefun triingf owt theBin gspeller by typying a sentance or clcking teh sampels bellow';
+
+        // Form initializations
+        var htmlPrefix_ms = '<div class="chart-html-popover"><img src="images/logo-ms-cognitive-services.png" alt="Angry Nun" width="75">';
+        var htmlSuffix = '</div>';
+        $scope.htmlPopoverIncidentReport = $sce.trustAsHtml(htmlPrefix_ms + "This is a popular style for presenting stock high-low-close data. Did you know that most markets as well as providers like Yahoo provide free API's to securities market data?" + htmlSuffix);
 
 
         ctrl.setNemoinfo = function() {
