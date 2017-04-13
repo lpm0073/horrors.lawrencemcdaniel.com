06c32570e680b40b0e6c485c224a21387c4dbaed
Lawrence McDaniel <lpm0073@gmail.com>
2017-04-13 18:57:58 -0500

    trouble shoot prettify formatting in directive

--- src/common/common.prettyprinter.controller.js
+++ src/common/common.prettyprinter.controller.js
@@ -5,23 +5,24 @@
     .controller('PrettyPrinterController', PrettyPrinterController)
     .directive('prettyPrinter', PrettyPrinter);
 
-    PrettyPrinterController.$inject = ['$scope', '$http'];
-    function PrettyPrinterController($scope, $http) {
+    PrettyPrinterController.$inject = ['$scope', '$http', '$sce'];
+    function PrettyPrinterController($scope, $http, $sce) {
         console.log('PrettyPrinterController instantiated', $scope.sourceCode);
         var ctrl = this;
 
         ctrl.sourceCode = $scope.sourceCode;
+
         var promise = $http.get($scope.sourceCode);
 
         promise.then(
           function success(response) {
-            ctrl.source_code = response.data;
+            ctrl.source_code = $sce.trustAsHtml(response.data);
+            ctrl.succeeded = true;
             console.log('http promise success.');
           },
           function failure(response) {
             console.log('http promise failure. something terrible happened: ', response);
           });
-
 
     }
 
