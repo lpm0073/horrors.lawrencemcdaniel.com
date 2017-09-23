88bf14a946e94d892e136e574ce28de48c62f89a
Lawrence McDaniel <lpm0073@gmail.com>
2017-09-23 15:00:39 -0500

    change d3 js lib to https

--- src/public/d3/d3.factory.js
+++ src/public/d3/d3.factory.js
@@ -20,7 +20,7 @@ angular.module('d3', [])
     var scriptTag = $document[0].createElement('script');
     scriptTag.type = 'text/javascript';
     scriptTag.async = false;
-    scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+    scriptTag.src = 'https://d3js.org/d3.v3.min.js';
     scriptTag.onreadystatechange = function () {
       if (this.readyState == 'complete') onScriptLoad();
     }
